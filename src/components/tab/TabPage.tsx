641494fef43db93a5c1bb3c09b02945445cbb5c9
LEEZY <leezy97html@gmail.com>
2022-10-06 17:51:54 +0900

    Add Tab page (DonutChart + Accodion)

--- src/components/tab/TabPage.tsx
+++ src/components/tab/TabPage.tsx
@@ -5,6 +5,8 @@ import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
+import Accodion from '../accodion/Accodion';
+import DonutChart from '../chart/DonutChart';
 
 export default function LabTabs() {
   const [value, setValue] = React.useState('1');
@@ -23,7 +25,10 @@ export default function LabTabs() {
             <Tab label="Item Three" value="3" />
           </TabList>
         </Box>
-        <TabPanel value="1">Item One</TabPanel>
+        <TabPanel value="1">
+          <DonutChart />
+          <Accodion />
+        </TabPanel>
         <TabPanel value="2">Item Two</TabPanel>
         <TabPanel value="3">Item Three</TabPanel>
       </TabContext>
