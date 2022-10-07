bebeaa944baf42b849506249c6bb80a30be49c46
LEEZY <leezy97html@gmail.com>
2022-10-07 17:44:53 +0900

    Add Mui Grid table

---
+++ src/pages/toast-ui/mui-grid2.tsx
@@ -0,0 +1,16 @@
+import { Container } from "@mui/system";
+import ResponsiveGrid from "../../components/dataGrid/RespnsiveGrid";
+
+
+
+const PageMuiGrid2 = () => {
+    return (
+        <>
+            <Container >
+                <ResponsiveGrid/>
+			</Container>		
+        </>
+    )
+};
+
+export default PageMuiGrid2;
