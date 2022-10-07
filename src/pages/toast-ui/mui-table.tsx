cc76440fcc860912705a624f4f1ea463049f8af7
LEEZY <leezy97html@gmail.com>
2022-10-07 17:42:55 +0900

    Add Collapsible Table

---
+++ src/pages/toast-ui/mui-table.tsx
@@ -0,0 +1,16 @@
+import { Container } from "@mui/system";
+import CollapsibleTable from "../../components/dataGrid/CollapsibleTable";
+
+
+
+const PageMuiGrid = () => {
+    return (
+        <>
+            <Container >
+                <CollapsibleTable />
+			</Container>		
+        </>
+    )
+};
+
+export default PageMuiGrid;
